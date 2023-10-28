3e68f3d5d64efc34ec04e9c5589a004a6e665848
Silvan Josten <silvanjosten@live.de>
2023-10-28 19:03:38 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const PORT = 3000;
+const distPath = path.join(__dirname, '../dist');
+
+const app = express();
+
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
+// Serve static files from the Vue app (assuming a "dist" folder)
+app.use(express.static(distPath));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
